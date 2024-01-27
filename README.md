b2c364ceb3fbb3392dc8c313c0937a68924b913a
Cainã Portilho <cainportilho@gmail.com>
2024-01-27 20:43:40 -0300

    deploy

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Adivinhas números
+# Adivinhas numeros
+
+## Objetivo do jogo
+Nesse jogo, é gerado um número entre 0 e 100. O seu objetivo é tentar acertar, por tentativa e erro, qual foi o número gerado. Durante o jogo, caso você erre o número, aparece uma dica se o número gerado é maior ou menor que o número que você digitou como resposta. No final, com o acerto do número digitado, aparece quantas tentativas você precisou e se gostaria de jogar mais uma vez.
+
+## Objetivo do desenvolvimento
+O objetivo para o desenvolvimento desse jogo foi treinar a lógica de programação. Algumas das coisas implementadas foram:
+
+- Estrutura de repetição
+- Estruturas de condição
+- Recursividade
+- Funções
+- Entrada e saída de dados
+- Validação de dados
+
+## Instalação
+Para poder jogar, você deve ter o Python instalado. Com o Python instalado, você precisa clonar o repositório do GitHub ou baixar o zip. Feito isso, com o terminal aberto dentro da pasta do repositório, execute o seguinte comando:
+~~~
+pip install requirements.txt
+~~~
+Esse comando baixa as bibliotecas necessárias para o jogo funcionar. Agora, basta rodar o jogo com o comando:
+~~~
+python main.py
+~~~
---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+import os
+from art import text2art
+from colorama import Fore
+
+init_game = ''
+
+def menu():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+    name_game = text2art('Adivinhas Numeros', font='small', chr_ignore=True)
+    print(Fore.GREEN + name_game + Fore.RESET)
+    print(Fore.BLUE, 'Neste jogo, o seu objetivo é acertar o número gerado aleatoriamente entre 0 e 100.\n Pressione "s" (start) para começar ou "e" (exit) para sair.', Fore.RESET)
+def number_generate():
+    num = random.randint(1, 100)
+    return num 
+def game():
+    number = number_generate()
+    tentativas = 1
+
+    while True:
+        input_number = input(' Digite um número: ')
+        if not input_number.isdigit():
+            print(Fore.MAGENTA, 'O valor digitado não é um número!', Fore.RESET)
+        elif number == int(input_number):
+            print(Fore.GREEN, 'Você acertou!', Fore.RESET)
+            print(Fore.YELLOW, f'Número de tentativas: {tentativas}', Fore.RESET)
+                
+            init_new_game = input(' Gostaria de jogar novamente? Digite "s" (start) para começar ou "e" (exit) para sair.\n ').lower()
+            if init_new_game == 's':
+                game()
+            else:
+                exit()
+        elif number > int(input_number):
+            print(Fore.RED, 'Digite um número maior.', Fore.RESET)
+        elif number < int(input_number):
+            print(Fore.CYAN, 'Digite um número menor.', Fore.RESET)
+        tentativas += 1  
+menu()
+
+while init_game not in ['s', 'e']:
+    init_game = input('\n Digite uma opção: ').lower()
+
+if init_game == 's':
+    game()
+else:
+    exit()
